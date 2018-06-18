b391c6002d354c595531301e3817b4ed322a8c4c
nadav-meidan <nadav.medan@mail.huji.ac.il>
2018-06-19 01:04:04 +0300

    Added README, not finished.

--- src/oop/ex6/regex/Regex.java
+++ src/oop/ex6/regex/Regex.java
@@ -1,57 +1,55 @@
 package oop.ex6.regex;
 
-import oop.ex6.method.Method;
-import oop.ex6.variable.Variable;
-
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+/**
+ * This class holds the regular expressions that the program uses to verify the code correctness.
+ */
 public class Regex {
 
-    // ---------- GENERAL ----------
+    /**************************************Genera ************************************************/
 
-	/** optional space */
+    /** optional space */
     public static final String OPTIONAL_SPACE = "\\s*";
-	/** at least one space. */
+    /** at least one space. */
     public static final String AT_LEAST_ONE_SPACE = "\\s+";
-	/** optional final. */
+    /** optional final. */
     public static final String OPTIONAL_FINAL = "((final)" + AT_LEAST_ONE_SPACE + ")?";
 
     /** legal variable name */
     public static final String LEGAL_VARIABLE_NAME = "([a-zA-Z]+[\\w]*|_+[\\w]+)";
-	/** legal method name */
+    /** legal method name */
     public static final String LEGAL_METHOD_NAME = "([a-zA-Z]+[\\w]*)";
 
     /** open bracket */
     public static final String OPEN_BRACKET = "\\(";
-	/** close bracket */
+    /** close bracket */
     public static final String CLOSE_BRACKET = "\\)";
-	/** open scope */
+    /** open scope */
     public static final String OPEN_SCOPE = "\\{";
-	/** close scope */
+    /** close scope */
     public static final String CLOSE_SCOPE = "\\}";
 
     /** semicolon */
     public static final String SEMICOLON = ";";
-	/** equals */
+    /** equals */
     public static final String EQUALS = "=";
-	/** comment */
-	public static final String COMMENT = "\\/\\/";
+    /** comment */
+    public static final String COMMENT = "\\/\\/";
 
-	/** void */
+    /** void */
     public static final String VOID = "void";
-	/** return */
+    /** return */
     public static final String RETURN = "return";
 
-	/** comment line */
+    /** comment line */
     public static final String COMMENT_LINE = COMMENT + ".*";
-	/** empty line */
+    /** empty line */
     public static final String EMPTY_LINE = OPTIONAL_SPACE;
     /** close scope line */
     public static final String CLOSE_SCOPE_LINE = OPTIONAL_SPACE + CLOSE_SCOPE + OPTIONAL_SPACE;
 
-    // ---------- TYPES ----------
+    /**************************************Types ************************************************/
 
     public static final String INT = "(int)";
     public static final String DOUBLE = "(double)";
@@ -61,24 +59,25 @@ public class Regex {
 
     public static final String VARIABLE_TYPE = "(int|double|boolean|String|char)";
 
-    // ---------- LEGAL VALUES ----------
+    /**************************************Legal Values ******************************************/
 
-	/** int legal value */
+    /** int legal value */
     public static final String LEGAL_INT_VALUE = "-?\\d+";
-	/** double legal value */
+    /** double legal value */
     public static final String LEGAL_DOUBLE_VALUE = "-?\\d+([.](\\d)+)?";
-	/** char legal value */
+    /** char legal value */
     public static final String LEGAL_CHAR_VALUE = "'.'";
-	/** String legal value */
+    /** String legal value */
     public static final String LEGAL_STRING_VALUE = "\\\".*\\\"";
-	/** boolean legal value */
+    /** boolean legal value */
     public static final String LEGAL_BOOLEAN_VALUE = "((true|false)|" + LEGAL_DOUBLE_VALUE + ")";
-	/** legal value */
+    /** legal value */
     public static final String LEGAL_VALUE = "(" + LEGAL_INT_VALUE + "|" + LEGAL_DOUBLE_VALUE + "|" +
             LEGAL_CHAR_VALUE + "|" + LEGAL_STRING_VALUE + "|" + LEGAL_BOOLEAN_VALUE + "|" + LEGAL_VARIABLE_NAME + ")";
 
-	/** map with the legal values */
-	public static final HashMap<String, String> legalValue = new HashMap<>();
+    /** map with the legal values */
+    public static final HashMap<String, String> legalValue = new HashMap<>();
+
     static {
         legalValue.put("int", LEGAL_INT_VALUE);
         legalValue.put("double", LEGAL_DOUBLE_VALUE);
@@ -88,56 +87,56 @@ public class Regex {
     }
 
 
-    // ---------- VARIABLE DECLARATION ----------
+    /***********************************Variable Declaration *****************************************/
 
-	/** variable declaration */
+    /** variable declaration */
     public static final String VARIABLE_DECLARATION =
             OPTIONAL_SPACE + OPTIONAL_FINAL + OPTIONAL_SPACE + VARIABLE_TYPE + AT_LEAST_ONE_SPACE +
                     "(((" + LEGAL_VARIABLE_NAME + ")|(" + LEGAL_VARIABLE_NAME + ")" + OPTIONAL_SPACE +
-		            EQUALS + OPTIONAL_SPACE + LEGAL_VALUE + ")" + OPTIONAL_SPACE + "(" + OPTIONAL_SPACE + "," +
-		            OPTIONAL_SPACE + "((" + LEGAL_VARIABLE_NAME + ")|(" + LEGAL_VARIABLE_NAME + ")" +
-		            OPTIONAL_SPACE + EQUALS + OPTIONAL_SPACE + LEGAL_VALUE + ")" + ")*" + OPTIONAL_SPACE + ")" +
-		            SEMICOLON + OPTIONAL_SPACE;
+                    EQUALS + OPTIONAL_SPACE + LEGAL_VALUE + ")" + OPTIONAL_SPACE + "(" + OPTIONAL_SPACE + "," +
+                    OPTIONAL_SPACE + "((" + LEGAL_VARIABLE_NAME + ")|(" + LEGAL_VARIABLE_NAME + ")" +
+                    OPTIONAL_SPACE + EQUALS + OPTIONAL_SPACE + LEGAL_VALUE + ")" + ")*" + OPTIONAL_SPACE + ")" +
+                    SEMICOLON + OPTIONAL_SPACE;
 
-	/** variable assignment */
+    /** variable assignment */
     public static final String VARIABLE_ASSIGNMENT = OPTIONAL_SPACE + "(" + LEGAL_VARIABLE_NAME + ")" +
-		    OPTIONAL_SPACE + EQUALS + OPTIONAL_SPACE + "(" + LEGAL_VALUE + ")" + OPTIONAL_SPACE + SEMICOLON +
-		    OPTIONAL_SPACE;
+            OPTIONAL_SPACE + EQUALS + OPTIONAL_SPACE + "(" + LEGAL_VALUE + ")" + OPTIONAL_SPACE + SEMICOLON +
+            OPTIONAL_SPACE;
 
-	/** variable optional assignment */
-	public static final String VARIABLE_OPTIONAL_ASSIGNMENT = OPTIONAL_SPACE + "(" + LEGAL_VARIABLE_NAME + ")" +
-			OPTIONAL_SPACE + "(" + EQUALS + OPTIONAL_SPACE + "(" + LEGAL_VALUE + "))?" + OPTIONAL_SPACE;
+    /** variable optional assignment */
+    public static final String VARIABLE_OPTIONAL_ASSIGNMENT = OPTIONAL_SPACE + "(" + LEGAL_VARIABLE_NAME + ")" +
+            OPTIONAL_SPACE + "(" + EQUALS + OPTIONAL_SPACE + "(" + LEGAL_VALUE + "))?" + OPTIONAL_SPACE;
 
 
-    // ---------- IF, WHILE ----------
+    /**************************************If / While ************************************************/
 
-	/** if or while */
+    /** if or while */
     public static final String IF_WHILE = OPTIONAL_SPACE + "(if|while)" + OPTIONAL_SPACE + OPEN_BRACKET +
             "(" + OPTIONAL_SPACE + "(" + LEGAL_BOOLEAN_VALUE + "|" + LEGAL_VARIABLE_NAME + ")" + OPTIONAL_SPACE +
-			"((&&|\\|\\|)" + OPTIONAL_SPACE + "(" + LEGAL_BOOLEAN_VALUE + "|" + LEGAL_VARIABLE_NAME + ")" +
-			OPTIONAL_SPACE + ")*)" + CLOSE_BRACKET + OPTIONAL_SPACE + OPEN_SCOPE + OPTIONAL_SPACE;
+            "((&&|\\|\\|)" + OPTIONAL_SPACE + "(" + LEGAL_BOOLEAN_VALUE + "|" + LEGAL_VARIABLE_NAME + ")" +
+            OPTIONAL_SPACE + ")*)" + CLOSE_BRACKET + OPTIONAL_SPACE + OPEN_SCOPE + OPTIONAL_SPACE;
 
-    // ---------- METHOD ----------
+    /**************************************Method ************************************************/
 
-	/** method parameter */
-	public static final String PARAMETER = OPTIONAL_SPACE + OPTIONAL_FINAL + OPTIONAL_SPACE + VARIABLE_TYPE +
+    /** method parameter */
+    public static final String PARAMETER = OPTIONAL_SPACE + OPTIONAL_FINAL + OPTIONAL_SPACE + VARIABLE_TYPE +
             AT_LEAST_ONE_SPACE + "(" + LEGAL_VARIABLE_NAME + ")" + OPTIONAL_SPACE;
-	/** list of parameters */
+    /** list of parameters */
     public static final String LIST_OF_PARAMETERS = "((" + PARAMETER + ")" + "(," + PARAMETER + ")*)?";
-	/** variable sent to method */
+    /** variable sent to method */
     public static final String VARIABLE_TO_METHOD = "(" + LEGAL_VARIABLE_NAME + "|" + LEGAL_VALUE + ")";
-	/** list of variables */
+    /** list of variables */
     public static final String LIST_OF_VARIABLES = OPTIONAL_SPACE + "((" + VARIABLE_TO_METHOD + ")" +
             OPTIONAL_SPACE + "(," + OPTIONAL_SPACE + VARIABLE_TO_METHOD + OPTIONAL_SPACE + ")*" + ")?" +
             OPTIONAL_SPACE;
-	/** method */
+    /** method */
     public static final String METHOD = OPTIONAL_SPACE + VOID + AT_LEAST_ONE_SPACE + LEGAL_METHOD_NAME +
             OPTIONAL_SPACE + OPEN_BRACKET + OPTIONAL_SPACE + LIST_OF_PARAMETERS +
             OPTIONAL_SPACE + CLOSE_BRACKET + OPTIONAL_SPACE + OPEN_SCOPE + OPTIONAL_SPACE;
-	/** call to method */
+    /** call to method */
     public static final String CALL_TO_METHOD = OPTIONAL_SPACE + LEGAL_METHOD_NAME + OPTIONAL_SPACE + OPEN_BRACKET +
             LIST_OF_VARIABLES + CLOSE_BRACKET + OPTIONAL_SPACE + SEMICOLON + OPTIONAL_SPACE;
-	/** return line */
+    /** return line */
     public static final String RETURN_LINE = OPTIONAL_SPACE + RETURN + OPTIONAL_SPACE + SEMICOLON + OPTIONAL_SPACE;
 }
 
